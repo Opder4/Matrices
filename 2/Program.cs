21024c04d29f02ce90e8ef0afd7e8cae008800e2
Emanuel Ramírez <er6348100@gmail.com>
2026-04-17 11:54:43 -0600

    Fin ej 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,72 @@
-﻿Console.WriteLine("");
+﻿int filas = 0;
+int columnas = 0;
+int valor = 0;
+int mas = 1;
+int buscar = 0;
+
+Console.WriteLine("Ingrese Cuantas filas quiere");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out filas) || filas <= 0)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine("");
+Console.WriteLine("Ingrese Cuantas Columnas quiere");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out columnas) || columnas <= 0)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine();
+int[,] numeros = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Ingrese el {mas}° valor");
+        Console.WriteLine();
+        while (!int.TryParse(Console.ReadLine(), out valor))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dato no válido");
+            Console.WriteLine();
+        }
+        numeros[i, j] = valor;
+        mas++;
+        ;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine();
+
+Console.WriteLine($"Ingrese una fila para mostrar (0 - {filas-1})");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out buscar) || buscar<0 || buscar>=filas)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Los números en esa fila son: ");
+Console.WriteLine();
+for  (int i = 0; i < columnas; i++)
+{
+    Console.Write(numeros[buscar, i] + " ");
+}
+Console.WriteLine();
