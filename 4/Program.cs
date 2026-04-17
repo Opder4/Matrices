52ebffd80e6cd0d8698a44d74eb163225affe343
Emanuel Ramírez <er6348100@gmail.com>
2026-04-17 12:01:35 -0600

    Fin ej 4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,1 +1,68 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int filas = 0;
+int columnas = 0;
+int valor = 0;
+int mas = 1;
+
+Console.WriteLine("Ingrese Cuantas filas quiere");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out filas) || filas <= 0)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine("");
+Console.WriteLine("Ingrese Cuantas Columnas quiere");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out columnas) || columnas <= 0)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine();
+int[,] numeros = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Ingrese el {mas}° valor");
+        Console.WriteLine();
+        while (!int.TryParse(Console.ReadLine(), out valor))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dato no válido");
+            Console.WriteLine();
+        }
+        numeros[i, j] = valor;
+        mas++;
+        ;
+    }
+}
+int mayor = numeros[0, 0];
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("El número mayor en la matriz es:");
+Console.WriteLine();
+for  (int i = 0;i < filas; i++)
+{
+    for(int j = 0;j < columnas; j++)
+    {
+        if (mayor < numeros[i,j])
+        {
+            mayor=numeros[i,j];
+        }
+    }
+}
+Console.WriteLine(mayor);
